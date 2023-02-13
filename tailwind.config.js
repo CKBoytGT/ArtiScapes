91b58d95fdf25c9200cde284fbb16cbf0d0d6587
C K Boyt <ckboyt@gmail.com>
2023-02-13 14:40:34 -0500

    Fix Fugaz One font name

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     fontFamily: {
       'sans': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-      'display': ['Fugaz One', 'cursive']
+      'display': ['"Fugaz One"', 'cursive']
     }
   },
   plugins: [],

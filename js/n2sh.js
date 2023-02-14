2c2aed013b53823264a5e48cbf52252bb3507176
C K Boyt <ckboyt@gmail.com>
2023-02-13 22:02:26 -0500

    Typo fix

--- js/n2sh.js
+++ js/n2sh.js
@@ -1,2 +1,2 @@
-const googleKey="AIzaSyAmUPLJdyPE8KF8uOxxt1hNUZIDE3ZVaCE";
-const unsplashKey="q2jCEvi971aOZUDT7NzShhejthh62UnWkjoxMCI9DrU";
+const googleKey = "AIzaSyAmUPLJdyPE8KF8uOxxt1hNUZIDE3ZVaCE";
+const unsplashKey = "q2jCEvi971aOZUDT7NzShhejthh62UnWkjoxMCI9DrU";
